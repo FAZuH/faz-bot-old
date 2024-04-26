437e3793841ae768aea7e5b4decf14f6dbe66647
FAZuH <fazuhhh@gmail.com>
2024-04-26 17:02:30 +0700

    style: fixed ErrorHandler.py typehints

--- fazbot/util/error_handler.py
+++ fazbot/util/error_handler.py
@@ -2,9 +2,10 @@ from functools import wraps
 from inspect import iscoroutinefunction
 from typing import Awaitable, Callable, ParamSpec, TypeVar, Iterable
 
-from fazbot import Logger
+from fazbot.logger import ConsoleLogger
 
 T = TypeVar('T')
+U = TypeVar('U')
 P = ParamSpec('P')
 
 
@@ -13,22 +14,22 @@ class ErrorHandler:
     @staticmethod
     def retry_decorator(max_retries: int, exceptions: type[BaseException] | Iterable[type[BaseException]]) -> Callable[[Callable[P, T]], Callable[P, Awaitable[T]]]:
         """ Retries the wrapped function/method `times` times if the exceptions listed in `exceptions` are thrown """
-
         def decorator(f: Callable[P, T]) -> Callable[P, Awaitable[T]]:
-
             @wraps(f)
             async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:  # type: ignore
-                for _ in range(max_retries + 1):
+                for _ in range(max_retries):
                     try:
-                        return (await f(*args, **kwargs)) if iscoroutinefunction(f) else f(*args, **kwargs)
-                    except exceptions:
-                        # FazBotLogger.get_console().exception(
-                        #     f"{f.__qualname__} failed. Raising exception to main thread...\n"
-                        #     f"args:{str(args)[:30]}\n"
-                        #     f"kwargs:{str(kwargs)[:30]}"
-                        # )
-                        raise
-
+                        return await ErrorHandler._must_return(f)
+                    except exceptions:  # type: ignore
+                        ConsoleLogger.exception(
+                                f"{f.__qualname__} failed. Retrying..."
+                                f"args:{str(args)[:30]}\n"
+                                f"kwargs:{str(kwargs)[:30]}"
+                        )
+                return await ErrorHandler._must_return(f)
             return wrapper
-
         return decorator
+
+    @staticmethod
+    async def _must_return(func: Callable[P, T] | Callable[P, Awaitable[T]]) -> T:
+        return await func() if iscoroutinefunction(func) else func()  # type: ignore
