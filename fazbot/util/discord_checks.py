1e99ec5ffe3ebb79e2b40a4a40399cbba6820b8b
FAZuH <fazuhhh@gmail.com>
2024-04-27 23:46:17 +0700

    refactor: removed ctx.send() on DiscordChecks.is_admin()

--- fazbot/util/discord_checks.py
+++ fazbot/util/discord_checks.py
@@ -13,11 +13,10 @@ class DiscordChecks:
     def __init__(self, app: Core) -> None:
         self._app = app
 
-    async def is_admin(self, ctx: commands.Context[Any]) -> bool:
+    def is_admin(self, ctx: commands.Context[Any]) -> bool:
         is_admin = ctx.author.id == self._app.config.application.admin_discord_id
-        if not is_admin:
-            await ctx.send("You don't have permission to use this command.")
         return is_admin
 
     def is_not_banned(self, ctx: commands.Context[Any]) -> bool:
-        return ctx.author.id not in self._app.userdata.get(UserdataFile.BANNED_USERS)
+        is_not_banned = ctx.author.id not in self._app.userdata.get(UserdataFile.BANNED_USERS)
+        return is_not_banned
