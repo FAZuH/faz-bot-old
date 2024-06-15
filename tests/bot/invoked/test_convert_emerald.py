3310111074c1d38147ee74440d302d98e016330a
FAZuH <fazuhhh@gmail.com>
2024-06-15 08:14:35 +0700

    test: add more TestConvertEmerald tests

--- tests/bot/invoked/test_convert_emerald.py
+++ tests/bot/invoked/test_convert_emerald.py
@@ -1,7 +1,7 @@
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import AsyncMock, MagicMock
 
-from fazbot.bot.invoke import InvokeConvertEmerald
+from fazbot.bot.invoke import Asset, InvokeConvertEmerald
 
 
 class TestConvertEmerald(IsolatedAsyncioTestCase):
@@ -9,11 +9,47 @@ class TestConvertEmerald(IsolatedAsyncioTestCase):
     def setUp(self) -> None:
         self.interaction = AsyncMock()
         self.asset = MagicMock()
+
         self.obj = InvokeConvertEmerald(self.interaction, "100le")
         self.obj.set_assets(self.asset)
-        return super().setUp()
+    
+    def test_set_assets(self) -> None:
+        # PREPARE
+        file = MagicMock()
+        filename = "liquidemerald"
 
-    async def test_convert_emerald(self) -> None:
+        # ACT
+        self.obj.set_assets({filename: file})
+
+        # ASSERT
+        self.assertEqual(file, self.obj.ASSET_LIQUIDEMERALD.file)
+        self.assertEqual(filename, self.obj.ASSET_LIQUIDEMERALD.filename)
+
+    async def test_run(self) -> None:
+        # PREPARE
+        embed = MagicMock()
+        self.obj._get_embed = MagicMock(return_value=embed)
+
+        # ACT
         await self.obj.run()
 
+        # ASSERT
+        self.obj._interaction.send.assert_called_once_with(embed=embed, file=self.obj.ASSET_LIQUIDEMERALD.file)
 
+    def test_get_embed(self) -> None:
+        # PREPARE
+        self.interaction.user.display_name = "name"
+        self.interaction.user.display_avatar.url = "url"
+
+        # ACT 
+        embed = self.obj._get_embed(self.interaction, self.obj._emeralds)
+        
+        # ASSERT
+        self.assertEqual(embed.title, "Emerald Convertor")
+        self.assertEqual(embed.color.value, 8894804)  # type: ignore
+        self.assertEqual(embed.description, "Converted: **1stx 36le 0eb 0e**\nEmeralds Total: **409600e**")
+        self.assertEqual(embed.fields[0].name, "TM Set Price")
+        self.assertEqual(embed.fields[1].name, "Silverbull Set Price")
+        self.assertEqual(embed.author.name, "name")
+        self.assertEqual(embed.author.icon_url, "url")
+        self.assertEqual(embed.thumbnail.url, "attachment://liquidemerald")
