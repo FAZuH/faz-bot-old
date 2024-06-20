0d19b3dfb15ea3161318ef0409f3e90189b6bf66
FAZuH <fazuhhh@gmail.com>
2024-06-21 03:40:52 +0700

    refactor(db): rename BannedUserRepository.is_exists to is_banned

--- fazbot/db/fazbot/repository/banned_user_repository.py
+++ fazbot/db/fazbot/repository/banned_user_repository.py
@@ -14,7 +14,7 @@ class BannedUserRepository(Repository[BannedUser]):
     def __init__(self, database: BaseAsyncDatabase) -> None:
         super().__init__(database, BannedUser)
 
-    async def is_exists(self, guild_id: int) -> bool:
+    async def is_banned(self, guild_id: int) -> bool:
         async with self.database.enter_session() as session:
             stmt = select(exists().where(self.get_model_cls().user_id == guild_id))
             result = await session.execute(stmt)
