0228049d5fc991fc559e480bd19865ad070e3f08
FAZuH <fazuhhh@gmail.com>
2024-06-22 07:56:59 +0700

    feat(db): add get_all_whitelisted_guild_ids

--- fazbot/db/fazbot/repository/whitelisted_guild_repository.py
+++ fazbot/db/fazbot/repository/whitelisted_guild_repository.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
+from sqlalchemy import select
+
 from ..model import WhitelistedGuild
 from ._repository import Repository
 
 if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
     from ... import BaseAsyncDatabase
 
 
@@ -12,3 +15,13 @@ class WhitelistedGuildRepository(Repository[WhitelistedGuild, int]):
 
     def __init__(self, database: BaseAsyncDatabase) -> None:
         super().__init__(database, WhitelistedGuild)
+
+    async def get_all_whitelisted_guilds(self, session: None | AsyncSession = None) -> list[int]:
+        model = self.get_model_cls()
+        async with self.database.must_enter_session(session) as session:
+            stmt = select(model)
+            result = await session.execute(stmt)
+            whitelisted_guilds = result.scalars().all()
+
+        whitelisted_guild_ids = [guild.guild_id for guild in whitelisted_guilds]
+        return whitelisted_guild_ids
