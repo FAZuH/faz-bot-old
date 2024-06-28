5b5859cb271c8489f14d34d4236bc06303614589
FAZuH <fazuhhh@gmail.com>
2024-06-28 15:52:51 +0700

    refactor: rename wynndb to fazdb

--- fazbot/config.py
+++ fazbot/config.py
@@ -18,14 +18,14 @@ class Config:
         self.discord_status_webhook = self.__must_get_env("DISCORD_STATUS_WEBHOOK")
 
         self.fazbot_db_max_retries = self.__must_get_env("FAZBOT_DB_MAX_RETRIES", int)
-        self.wynndb_db_max_retries = self.__must_get_env("WYNNDB_DB_MAX_RETRIES", int)
+        self.fazdb_db_max_retries = self.__must_get_env("FAZDB_DB_MAX_RETRIES", int)
 
         self.mysql_host = self.__must_get_env("MYSQL_HOST")
         self.mysql_port = self.__must_get_env("MYSQL_PORT", int)
         self.mysql_username = self.__must_get_env("MYSQL_USER")
         self.mysql_password = self.__must_get_env("MYSQL_PASSWORD")
         self.fazbot_db_name = self.__must_get_env("MYSQL_FAZBOT_DATABASE")
-        self.wynndb_db_name = self.__must_get_env("MYSQL_WYNNDB_DATABASE")
+        self.fazdb_db_name = self.__must_get_env("MYSQL_FAZDB_DATABASE")
 
     def __must_get_env[T](self, key: str, type_strategy: Callable[[str], T] = str) -> T:
         try:
