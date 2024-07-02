3e79c41802cdb075c602e9726bd21f9804723ae0
FAZuH <fazuhhh@gmail.com>
2024-07-02 21:42:47 +0700

    fix(bot): fix help cog calling removed method _respond_error

--- fazbot/bot/cog/help.py
+++ fazbot/bot/cog/help.py
@@ -2,8 +2,9 @@ from typing import Any
 
 import nextcord
 
-from ..invoke import InvokeHelp as InvokeHelp
 from . import CogBase
+from .. import CommandFailure
+from ..invoke import InvokeHelp as InvokeHelp
 
 
 class Help(CogBase):
@@ -11,7 +12,7 @@ class Help(CogBase):
     @nextcord.slash_command(name="help", description="Help command")
     async def _help(self, interaction: nextcord.Interaction[Any]) -> None:
         if not interaction.guild:
-            return await self._respond_error(interaction, "You can only use this command in a guild channel.")
+            raise CommandFailure(interaction, "You can only use this command in a guild channel.")
 
         cmds = [cmd for cmd in interaction.guild.get_application_commands()]
         await InvokeHelp(interaction, cmds).run()
