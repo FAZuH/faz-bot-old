3aab64739999473c66ab3c5ff26e62f94730bc2f
FAZuH <fazuhhh@gmail.com>
2024-07-14 22:58:32 +0700

    refactor(app): Add create_db methods to App
    Marked enter_db methods as deprecated.

--- fazbot/app/app.py
+++ fazbot/app/app.py
@@ -4,6 +4,7 @@ from threading import Lock
 from typing import Generator
 
 from loguru import logger
+from typing_extensions import deprecated
 
 from fazbot.bot import Bot
 from fazbot.db import FazbotDatabase, FazdbDatabase, MangaNotifyDatabase
@@ -22,27 +23,9 @@ class App:
         p.setup()
         LoggerSetup.setup(p.LOG_DIR, p.DISCORD_LOG_WEBHOOK, p.ADMIN_DISCORD_ID)
 
-        self._fazbot_db = FazbotDatabase(
-            p.MYSQL_USERNAME,
-            p.MYSQL_PASSWORD,
-            p.MYSQL_HOST,
-            p.MYSQL_PORT,
-            p.FAZBOT_DB_NAME
-        )
-        self._fazdb_db = FazdbDatabase(
-            p.MYSQL_USERNAME,
-            p.MYSQL_PASSWORD,
-            p.MYSQL_HOST,
-            p.MYSQL_PORT,
-            p.FAZDB_DB_NAME
-        )
-        self._manga_notify_db = MangaNotifyDatabase(
-            p.MYSQL_USERNAME,
-            p.MYSQL_PASSWORD,
-            p.MYSQL_HOST,
-            p.MYSQL_PORT,
-            p.MANGA_NOTIFY_DB_NAME
-        )
+        self._fazbot_db = self.create_fazbot_db()
+        self._fazdb_db = self.create_fazdb_db()
+        self._manga_notify_db = self.create_manga_notify_db()
         self._bot = Bot(self)
 
     def start(self) -> None:
@@ -64,20 +47,53 @@ class App:
         with self._get_lock("bot"):
             yield self._bot
 
+    @deprecated("replace with create db")
     @contextmanager
     def enter_fazbot_db(self) -> Generator[FazbotDatabase]:
         with self._get_lock("fazbot_db"):
             yield self._fazbot_db
 
+    @deprecated("replace with create db")
     @contextmanager
     def enter_fazdb_db(self) -> Generator[FazdbDatabase]:
         with self._get_lock("fazdb_db"):
             yield self._fazdb_db
 
+    @deprecated("replace with create db")
     @contextmanager
     def enter_manga_notify_db(self) -> Generator[MangaNotifyDatabase]:
         with self._get_lock("manga_notify_db"):
             yield self._manga_notify_db
+
+    def create_fazbot_db(self) -> FazbotDatabase:
+        p = self.properties
+        return FazbotDatabase(
+            p.MYSQL_USERNAME,
+            p.MYSQL_PASSWORD,
+            p.MYSQL_HOST,
+            p.MYSQL_PORT,
+            p.FAZBOT_DB_NAME
+        )
+
+    def create_fazdb_db(self) -> FazdbDatabase:
+        p = self.properties
+        return FazdbDatabase(
+            p.MYSQL_USERNAME,
+            p.MYSQL_PASSWORD,
+            p.MYSQL_HOST,
+            p.MYSQL_PORT,
+            p.FAZDB_DB_NAME
+        )
+
+    def create_manga_notify_db(self) -> MangaNotifyDatabase:
+        p = self.properties
+        return MangaNotifyDatabase(
+            p.MYSQL_USERNAME,
+            p.MYSQL_PASSWORD,
+            p.MYSQL_HOST,
+            p.MYSQL_PORT,
+            p.MANGA_NOTIFY_DB_NAME
+        )
 
     def _get_lock(self, key: str) -> Lock:
         if key not in self._locks:
